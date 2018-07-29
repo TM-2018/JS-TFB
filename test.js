c0b08546121858846aadf536a0599d3d1352aefc
Tomek_M <tomasz.milaszewski@gmail.com>
2018-07-29 20:22:22 +0200

    Lesson 19 Loops example

--- test.js
+++ test.js
@@ -1,14 +1,7 @@
-for(i = 0; i < 10; i++) {
+var links = document.getElementsByTagName("a");
 
-    if (i===3 || i===5) {
-        continue;
-    }
+for (i = 0; i < links.length; i++) {
 
-    console.log(i);
-    
-    if (i===7) {
-        break;
-    }
+    links[i].className = "link-" + i;
+
 }
-
-console.log("I have broken out of the loop");
