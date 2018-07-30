69353daad5ad8ed4664dc617df78fd3b6943ea55
Tomek_M <tomasz.milaszewski@gmail.com>
2018-07-30 14:16:21 +0200

    Lesson 40 Onclick Event Example

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
-var Car = function(maxSpeed, driver){
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
-this.maxSpeed = maxSpeed;
-this.driver = driver;
-this.drive = function(speed, time){
-    console.log(speed * time);
+button.onclick = function(){
+
+    if(content.className == "open"){
+        //shrink the box
+        content.className = "";
+        button.innerHTML = "Show more";
+
+    } else {
+        //expand the box
+        content.className = "open";
+        button.innerHTML = "Show less";
+    }
+
+
 };
-this.logDriver = function(){
-    console.log("Driver name is " + this.driver);
-};
-
-}
-
-var myCar = new Car(70, "Ninja Man");
-var myCar2 = new Car(30, "Ninja Man 2");
-var myCar3 = new Car(50, "Ninja Man 3");
-
-myCar.drive(30,5);
-myCar3.logDriver();
